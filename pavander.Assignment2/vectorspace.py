32d04da149ff8bf04eb09a1c3866bff842d42e09
pavander <pavander@umich.edu>
2025-02-17 13:54:33 -0500

    indexDocument

--- pavander.Assignment2/vectorspace.py
+++ pavander.Assignment2/vectorspace.py
@@ -1,14 +1,76 @@
 # Paul Vander Woude (pavander)
 
+# Standard library imports
 import sys
-import tokenize
-import pathlib
+from pathlib import Path
 
-def indexDocument(doc_id, doc_weights, query_weights, inv_idx):
+# Third-party imports
+from stopwords import get_stopwords
+
+# Local application/library specific imports
+from preprocess import tokenizeText
+from porter_stemmer import PorterStemmer
+
+# Global variables
+STOP_WORDS = set(get_stopwords("en"))
+STEMMER = PorterStemmer()
+
+def preprocess(text, doc_id = None, verbose = 0):
+    """clean, tokenize, filter, and stem raw text."""
+    tokens_unfiltered = tokenizeText(text)
+    if (verbose >= 2):
+        print(f'tokenized {doc_id}')
+
+    tokens_unstemmed = [word for word in tokens_unfiltered if word not in STOP_WORDS]
+    if (verbose >= 2):
+        print(f'removed stop words from {doc_id}')
+
+    tokens = [STEMMER.stem(word) for word in tokens_unstemmed]
+    if (verbose >= 2):
+        print(f'stemmed {doc_id}')
+    
+    return tokens
+
+
+def tf_idf(tokens, inv_idx, doc_id, verbose = 0):
+    for token in tokens:
+        if token in inv_idx:
+            inv_idx[token][doc_id] += 1
+        else:
+            inv_idx[token] = {doc_id: 1}
+
     return inv_idx
 
 
-def retrieveDocuments(query, inv_idx, doc_weights, query_weights):
+def indexDocument(doc_id, dw_mode, qw_mode, inv_idx, verbose = 0):
+    """adds a document to the inverted index."""
+    # ==================================================
+    # STEP ONE: open file
+    doc_path = Path(doc_id)
+    if not doc_path.is_file():
+        raise(FileNotFoundError(f'could not find file {doc_id}'))
+    
+    with doc_path.open("r", encoding="ISO-8859-1") as doc:
+        if (verbose >= 2):
+            print(f'opened {doc_id}')
+    # ==================================================
+    # STEP TWO: preprocess (clean, tokenize, and stem)
+        doc_text_raw = doc.read()
+
+        tokens = preprocess(doc_text_raw, doc_id, verbose)
+        
+    
+    # ==================================================
+    # STEP THREE: add tokens to inverted index
+    if (dw_mode == 'tf.idf'):
+        inv_idx = tf_idf(tokens, inv_idx, doc_id, verbose)
+    else:
+        raise(ValueError(f'unimplemented mode(s): d: {dw_mode} q: {qw_mode}'))
+
+    return inv_idx
+
+
+def retrieveDocuments(query, inv_idx, dw_mode, qw_mode):
     results = dict()
     return results
 
@@ -21,17 +83,17 @@ def main():
 
     # check for sufficient arguments
     if (len(sys.argv) < 6):
-        print('USAGE: vectorspace.py DOC_WEIGHT_MODE QUERY_WEIGHT_MODE COSINE_NORM(T/F) DOCUMENT_DIR TEST_QUERY_FILE (VERBOSE(T/F))')
+        print('USAGE: vectorspace.py DOC_WEIGHT_MODE QUERY_WEIGHT_MODE COSINE_NORM(T/F) DOCUMENT_DIR TEST_QUERY_FILE (VERBOSE(0/1/2))')
         raise ValueError(f"Expected at least {5} arguments, got {len(sys.argv)}.")
 
     # argument 6: verbose flag
-    verbose = False
+    verbose = 0
     if (len(sys.argv) == 7):
-        verbose = (sys.argv[6] == 'T' | sys.argv[6] == 'True')
+        verbose = max(sys.argv[6],2)
 
     # check for extra arguments
     if (len(sys.argv) > 7):
-        print('USAGE: vectorspace.py DOC_WEIGHT_MODE QUERY_WEIGHT_MODE COSINE_NORM(T/F) DOCUMENT_DIR TEST_QUERY_FILE (VERBOSE(T/F))')
+        print('USAGE: vectorspace.py DOC_WEIGHT_MODE QUERY_WEIGHT_MODE COSINE_NORM(T/F) DOCUMENT_DIR TEST_QUERY_FILE (VERBOSE(0/1/2))')
         raise ValueError(f"Expected at most {6} arguments, got {len(sys.argv)}.")
     
     # argument 1: weighting scheme for documents
@@ -76,14 +138,14 @@ def main():
         raise(ValueError(f'unknown COSINE_NORM option: {sys.argv[3]} [expected: T/True OR F/False]'))
     
     # argument 4: document directory
-    doc_dir_path = pathlib.Path(sys.argv[4])
+    doc_dir_path = Path(sys.argv[4])
     if not doc_dir_path.is_dir():
         FileNotFoundError(f'The directory {sys.argv[4]} does not exist.')
     elif (verbose):
         print('document directory found successfully.')
 
     # argument 5: test query file
-    test_query_filepath = pathlib.Path(sys.argv[5])
+    test_query_filepath = Path(sys.argv[5])
     if not test_query_filepath.is_file():
         FileNotFoundError(f'The file {sys.argv[5]} does not exist.')
     elif (verbose):
